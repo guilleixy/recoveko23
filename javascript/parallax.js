32fd80b9c2ce9ddac1ee4b8e272721482bf64190
guille <129344574+guilleixy@users.noreply.github.com>
2023-10-25 19:46:47 +0200

    Add files via upload
    detalles esteticos

--- javascript/parallax.js
+++ javascript/parallax.js
@@ -3,3 +3,9 @@ const parallaxSmooth = document.querySelectorAll('.parallax-smooth');
 new simpleParallax(parallaxSmooth,{
     scale:1.2
 });
+
+const parallaxUltraSmooth = document.querySelectorAll('.parallax-ultra-smooth');
+
+new simpleParallax(parallaxUltraSmooth,{
+    scale:1.05
+});
